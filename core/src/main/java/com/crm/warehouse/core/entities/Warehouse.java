edd0d70a527d1595977f10a73facd83b5ef0b9b5
Eagle <lanselot4267@gmail.com>
2026-02-18 15:46:56 +0300

    добавил базовый слой

---
+++ core/src/main/java/com/crm/warehouse/core/entities/Warehouse.java
@@ -0,0 +1,34 @@
+package com.crm.warehouse.core.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "warehouse")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "warehouse_id")
+    private Integer id;
+
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+
+    @Column(nullable = false, name = "is_active")
+    private Boolean active;
+}
